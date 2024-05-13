a852f4b016f7bf950322b11bad16e6f7ba2ae5ce
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-05-13 20:36:20 +0200

    Adding the game.js file

--- scripts/game.js
+++ scripts/game.js
@@ -4,5 +4,35 @@ let game={
     playerMoves:[],
     choices:["button1","button2","button3","button4"]
 }
+function newGame(){
+    game.score=0;
+    game.playerMoves=[];
+    game.currentGame=[];
 
-module.exports={game};
+    //call show score at the end of new game
+    showScore();
+
+    //call add turn function
+    addTurn()
+}
+
+/**
+ * addTurn function is used to clear playerMoves, 
+ * choose random button and push it to the currentGame
+ * 
+ */
+function addTurn(){
+    //clear player moves
+    game.playerMoves=[];
+    //pushes a random button ID to the current game -> random between 0 and 3 
+    game.currentGame.push(game.choices[(Math.floor(Math.random()*4))]);
+    //call show turn 
+    //showTurn();
+}
+
+
+function showScore(){
+    document.getElementById("score").innerText=game.score;
+}
+
+module.exports={game,newGame,showScore,addTurn};
