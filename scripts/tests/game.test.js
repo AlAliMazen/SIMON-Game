b524ea9e68c5136745d44d11ca560ba53ec76dc6
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-05-13 20:06:08 +0200

    adding the showScore test function, showScore in JS file

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const {game}=require("../game");
+const {game,newGame,showScore}=require("../game");
 
 beforeAll(()=>{
     let fs=require("fs");
@@ -27,6 +27,43 @@ describe("game object contains correct keys",()=>{
     });
     test("choices contain correct ids",()=>{
         expect(game.choices).toEqual(["button1","button2","button3","button4"]);
+    }) 
+});
+
+/**
+ * checking the newGame function 
+ */
+
+describe("newGame works correctly",()=>{
+    beforeAll(()=>{
+        /**
+         * beforeAll is used to see that when calling newGame 
+         * function all data of the current game will be wiped out
+         * 
+         */
+        game.score=42;
+        game.currentGame=["button1","button2"];
+        game.playerMoves=["button3","button4"];
+
+        //function showScore 
+        document.getElementById("score").innerText="42";
+        newGame();
+    });
+    test("should reset game score to zero",()=>{
+        expect(game.score).toEqual(0)
+    });
+    /**
+     * either check the length of the array or 
+     * when it is empty
+     */
+    test("should clean player moves",()=>{
+        expect(game.playerMoves).toEqual([])
+    });
+    test("should clean current choices",()=>{
+        expect(game.currentGame.length).toBe(0);
+    });
+    test("should display 0 for the element with id of score",()=>{
+        expect(document.getElementById("score").innerText).toEqual(0);
     })
-    
+
 })
